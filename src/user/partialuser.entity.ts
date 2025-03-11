65e6a3fbe8336cdcc8beab7fa12b689306d82e3b
MBecker7 <marcbecker991@web.de>
2025-03-11 13:21:12 +0100

    Add partialuser and update patch endpoint

---
+++ src/user/partialuser.entity.ts
@@ -0,0 +1,25 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEmail, IsOptional } from "class-validator";
+import { Entity, BaseEntity } from "typeorm";
+
+@Entity()
+export class PartialUser extends BaseEntity  {
+    @ApiPropertyOptional({
+      example: 'newemail@example.com',
+    })
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+  
+    @ApiPropertyOptional({
+      example: 'NewPasswordHash123',
+    })
+    @IsOptional()
+    passwordHash?: string;
+  
+    @ApiPropertyOptional({
+      example: 'admin',
+    })
+    @IsOptional()
+    role?: 'user' | 'admin';
+}  
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { isEmpty } from 'class-validator';
+import { ApiBody, ApiParam, PartialType } from '@nestjs/swagger';
+import { PartialUser } from './partialuser.entity';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +15,7 @@ export class UserController {
     }
     
   @Get(':id')
+  @ApiParam({name: 'id', type: Number})
     readOne(
       @Param('id') id: number)
       {
@@ -19,6 +23,7 @@ export class UserController {
       }
 
   @Delete(':id')
+  @ApiParam({name: 'id', type: Number})
     delete(
       @Param('id') id: number)
       {
@@ -26,9 +31,22 @@ export class UserController {
       }
 
   @Post('create')
+  @ApiBody({ type: User })  
     create(
       @Body() newUser: User)
       {
         return this.userService.create(newUser);
       }
+  
+
+  @Patch(':id/update')
+  @ApiParam({name: 'id', type: Number})
+  @ApiBody({ type: PartialUser })
+      update(
+        @Param('id') id: number,
+        @Body() partialUser:PartialUser)
+        {
+          return this.userService.update(id , partialUser);
+        }
+
 }
