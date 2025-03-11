95b13759a14807f4370882c1dee2934f37201e8c
MBecker7 <marcbecker991@web.de>
2025-03-11 11:06:53 +0100

    Add swagger for api documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+    readAll(): Promise<User[]> {
+      return this.userService.readAll();
+    }
 }
